3b6e833a5e9e60c44f349275ef0720c6d6c6995c
romkravets <romann.kravets@gmail.com>
2019-07-08 15:52:55 +0300

    Added change in header

--- src/index/index.js
+++ src/index/index.js
@@ -1,27 +1,25 @@
 import './index.scss';
 
-// const scrollBtn = document.querySelector('.scroll__top');
+const scrollBtn = document.querySelector('.scroll__top');
 
 
 window.onscroll = () => {
    const navMenu = document.querySelector('#navMenu');
-   console.log(navMenu);
-   if(this.scrollY <= 10) navMenu.className = 'menu'; else navMenu.className = 'menu_scroll';
+   if(window.pageYOffset <= 10) navMenu.className = 'menu'; else navMenu.className = 'menu_scroll';
    scrollFunction();
  }
 
-//  const scrollFunction = () => {
-//    if(this.scrollY <= 1000) scrollBtn.className = ' '; else scrollBtn.className = 'scroll__top_active';
-//    console.log(scrollBtn);
-// }
+const scrollFunction = () => {
+   if(window.pageYOffset <= 1000) scrollBtn.className = ' '; else scrollBtn.className = 'scroll__top_active';
+   console.log(scrollBtn);
+}
 
-// const topFunction = () => {
-//    scrollBtn.addEventListener('click', () => {
-//       console.log('click');
-//       document.body.scrollTop = 0;
-//       document.documentElement.scrollTop = 0;
-//    });
-// }
+const topFunction = () => {
+   scrollBtn.addEventListener('click', () => {
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
+   });
+}
 
 const burgerFunction = () => {
    const burger = document.querySelector('.burger');
@@ -32,4 +30,4 @@ const burgerFunction = () => {
 }
 
 burgerFunction();
-// topFunction();
+topFunction();
