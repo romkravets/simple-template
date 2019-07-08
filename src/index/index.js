e5359a2baab461b16c2ddc212f032c4d7ea74295
romkravets <romann.kravets@gmail.com>
2019-07-08 14:18:32 +0300

    Added header

--- src/index/index.js
+++ src/index/index.js
@@ -1,2 +1,35 @@
 import './index.scss';
 
+// const scrollBtn = document.querySelector('.scroll__top');
+
+
+window.onscroll = () => {
+   const navMenu = document.querySelector('#navMenu');
+   console.log(navMenu);
+   if(this.scrollY <= 10) navMenu.className = 'menu'; else navMenu.className = 'menu_scroll';
+   scrollFunction();
+ }
+
+//  const scrollFunction = () => {
+//    if(this.scrollY <= 1000) scrollBtn.className = ' '; else scrollBtn.className = 'scroll__top_active';
+//    console.log(scrollBtn);
+// }
+
+// const topFunction = () => {
+//    scrollBtn.addEventListener('click', () => {
+//       console.log('click');
+//       document.body.scrollTop = 0;
+//       document.documentElement.scrollTop = 0;
+//    });
+// }
+
+const burgerFunction = () => {
+   const burger = document.querySelector('.burger');
+   const navItems = document.querySelector('.menu__items');
+   burger.addEventListener('click', () => {
+      navItems.classList.toggle('menu__items_opened');
+   });
+}
+
+burgerFunction();
+// topFunction();
